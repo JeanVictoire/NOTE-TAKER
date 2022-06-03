0c760b08ec7f43866fed9ca7f7a6a858d67ea6c5
JeanVictoire <jeanthecoders@gmail.com>
2022-06-03 02:00:13 -0400

    innitial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Note Taker
+
+![License: MIT](https://img.shields.io/static/v1?label=License&message=mit&color=blue)
+
+## Description
+
+An application called [Note Taker]() that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+The application will be invoked by using the following command: node server.js
+
+## Table of Contents
+
+- [Description](#Description)
+
+- [Installation](#Installation)
+
+- [Usage](#Usage)
+
+- [License](#License)
+
+- [Questions](#Questions)
+
+## Installation
+
+To install dependencies, run the following command: npm install
+
+## Usage
+
+Navigate to the folder of the project files and run node server.js in the commande line
+
+Open the website. Click the Get Started button. Enter your note's title and text into the appropriate fields.
+
+Click the Save icon in the navigation bar in the top right of the screen.
+
+## Screenshot
+
+Application preview:
+
+## Credits
+
+This project was created using [EXPRESS.JS](https://www.npmjs.com/package/express)
+
+- [Node.js](https://nodejs.org/en/)
+
+## Questions
+
+If you have any questions, please fell free to contact me:
+
+[Email](JeanTheCoders@gmail.com)
+
+[GitHub Profile](https://github.com/JeanVictoire)
+
+## License
+
+NOTE TAKER is licensed under [MIT](https://choosealicense.com/licenses/mit)
+
+Copyright (c) 2022 Jean Victoire
---
+++ server.js
@@ -0,0 +1,73 @@
+// Dependencies.
+const express = require('express');
+const uuid = require('uuid');
+const fs = require('fs');
+const path = require('path');
+
+// Express app.
+const app = express();
+
+// Set port.
+const PORT = process.env.PORT ||  3001;
+
+// Express middleware.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+// Notes route.
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+// api routes 
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname,  "./db/db.json"), 'utf8', (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+    });
+  });
+
+// Html route. 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+// Delete id.
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile(path.join(__dirname,  "./db/db.json"), 'utf8', (err, data) => {
+      let db = JSON.parse(data);
+      db = db.filter((e) => {
+        return e.id !== req.params.id;
+      });
+      fs.writeFile(
+        path.join(__dirname,  "./db/db.json"),
+        JSON.stringify(db, null, 2),
+        (err, data) => {
+          if (err) throw err;
+          res.json(db);
+        }
+      );
+    });
+  });
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname,  "./db/db.json"), 'utf8', (err, data) => {
+      let db = JSON.parse(data);
+      db.push({
+        id: uuid.v4(),
+        ...req.body,
+      });
+      fs.writeFile(
+        path.join(__dirname,  "./db/db.json"),
+        JSON.stringify(db, null, 2),
+        (err, data) => {
+          if (err) throw err;
+          res.json(db);
+        }
+      );
+    });
+  });
+ 
+//  Port. 
+app.listen(PORT, () => console.log(`The server is now listening on PORT ${PORT}`));
